77bd8c14e77958a05a1160ab0131434474da1016
Quan Nguyen <huu2uan@gmail.com>
2016-01-01 11:47:39 +0700

    day 1

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -8,3 +8,15 @@
                   (re-seq #"\)")
                   count)]
     (- up down)))
+
+(defn day1-position [s]
+  (loop [str s
+         up 0
+         down 0
+         pos 0]
+    (let [[c & rest] str]
+      (if (= -1 (- up down))
+        pos
+        (if (= \( c)
+          (recur rest (inc up) down (inc pos))
+          (recur rest up (inc down) (inc pos)))))))
