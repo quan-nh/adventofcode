07221f4e5c3ff64c263847e3c2ecb37c8de3626c
Quan Nguyen <huu2uan@gmail.com>
2016-01-17 00:55:29 +0700

    day 14

---
+++ src/adventofcode/day14.clj
@@ -0,0 +1,29 @@
+(ns adventofcode.day14
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input
+  (line-seq (io/reader (io/resource "day14"))))
+
+(defn distance [reindeer seconds]
+  (let [{:keys [speed time rest]} reindeer
+        a (quot seconds (+ time rest))
+        b (mod seconds (+ time rest))
+        c (min b time)]
+    (* (+ (* a time) c) speed)))
+
+(def reindeers
+  (->> input
+       (map #(str/replace % #"(can fly |km/s for |seconds, but then must rest for | seconds.$)" ""))
+       (map #(read-string (str "(" % ")")))
+       (map (fn [[_ speed time rest]] {:speed speed :time time :rest rest}))))
+
+(reduce max (map #(distance % 2503) reindeers))
+
+(->> (for [x (range 1 2504)]
+       (map #(distance % x) reindeers))
+     (map (fn [coll]
+            (let [max (apply max coll)]
+              (map #(if (= max %) 1 0) coll))))
+     (apply map +)
+     (reduce max))
