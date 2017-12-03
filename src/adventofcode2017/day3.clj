d99a4c464ad107b20c9557558bc9d3bd69963ffc
Quan Nguyen <huu2uan@gmail.com>
2017-12-03 23:02:07 +0700

    day3: better moving pattern, from balefrost

--- src/adventofcode2017/day3.clj
+++ src/adventofcode2017/day3.clj
@@ -3,61 +3,53 @@
 (def input 325489)
 
 ; part 1
-(defn next-pos [[x y]]
-  (let [abs-x (Math/abs x)
-        abs-y (Math/abs y)]
-    (cond
-      (and (= x y)
-           (neg? x))
-      [(inc x) y]
+(def north [0 1])
+(def east [1 0])
+(def south [0 -1])
+(def west [-1 0])
 
-      (and (= x y)
-           (pos? x))
-      [(dec x) y]
+(def direction-pattern
+  (cycle [east north west south]))
 
-      (and (zero? (+ x y))
-           (neg? x))
-      [x (dec y)]
+(def distance-pattern
+  (mapcat
+    (fn [x] [x x])
+    (iterate inc 1)))
 
-      (and (zero? (+ x y))
-           (>= x 0))
-      [(inc x) y]
+(def pattern
+  (mapcat
+    (fn [distance direction]
+      (repeat distance direction))
+    distance-pattern
+    direction-pattern))
 
-      (and (= abs-x (max abs-x abs-y))
-           (pos? x))
-      [x (inc y)]
+(defn next-pos [current-pos dir]
+  (mapv + current-pos dir))
 
-      (and (= abs-y (max abs-x abs-y))
-           (pos? y))
-      [(dec x) y]
-
-      (and (= abs-x (max abs-x abs-y))
-           (neg? x))
-      [x (dec y)]
-
-      (and (= abs-y (max abs-x abs-y))
-           (neg? y))
-      [(inc x) y])))
-
-(def grid (iterate next-pos [0 0]))
+(def grid
+  (reductions
+    next-pos
+    [0 0]
+    pattern))
 
 (let [[x y] (nth grid (dec input))]
   (+ (Math/abs x) (Math/abs y)))
 ; 552
 
 ;; part 2
-(defn total-around [m [x y]]
-  (+ (get m [x (inc y)] 0)
-     (get m [x (dec y)] 0)
-     (get m [(inc x) y] 0)
-     (get m [(inc x) (inc y)] 0)
-     (get m [(inc x) (dec y)] 0)
-     (get m [(dec x) y] 0)
-     (get m [(dec x) (inc y)] 0)
-     (get m [(dec x) (dec y)] 0)))
+(defn neighbors [[x y]]
+  (for [dx [-1 0 1]
+        dy [-1 0 1]
+        :when (not= 0 dx dy)]
+    [(+ x dx) (+ y dy)]))
+
+(defn pos-val [m pos]
+  (apply +
+         (for [p (neighbors pos)]
+           (get m p 0))))
 
 (reduce (fn [m [x y]]
-          (let [v (total-around m [x y])]
+          (let [v (pos-val m [x y])]
             (if (> v input)
               (reduced v)
               (assoc m [x y] v))))
