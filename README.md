073370d972ed93b10d76482aef748b6b4f93253f
q <huu2uan@gmail.com>
2022-12-01 11:28:51 +0700

    cleanup

--- README.md
+++ README.md
@@ -9,6 +9,3 @@ FIXME
 ## License
 
 Copyright © 2015 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject adventofcode "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/core.match "0.3.0-alpha4"]
