6bee18d5b29455486895e43eb6d174c5c000b0ee
q <huu2uan@gmail.com>
2022-12-03 09:01:17 +0700

    2022 day1

---
+++ src/aoc22/day01.nim
@@ -0,0 +1,18 @@
+import strutils
+
+var total, t1, t2, t3 = 0
+
+for calo in readFile("../../resources/2022/day01").splitLines:
+  if calo != "":
+    total += calo.parseInt
+  else:
+    if total >= t1:
+      (t1, t2, t3) = (total, t1, t2)
+    elif total >= t2:
+      (t2, t3) = (total, t2)
+    elif total >= t3:
+      t3 = total
+    total = 0
+
+echo t1
+echo t1 + t2 + t3
